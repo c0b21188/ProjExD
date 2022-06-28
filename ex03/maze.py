a9e173c3d7f4041c115c7cb36f334c03125dba7e
yamamoto <C0B21188e9@edu.teu.ac.jp>
2022-06-28 16:52:03 +0900

    4限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 import maze_maker as mm
 
 def key_down(event):
@@ -11,17 +12,17 @@ def key_up(event):
 
 def main_proc():
     global cx, cy, mx, my
+    UP, DOWN, LEFT, RIGHT = range(4)
+    KEY_DIC = {"w":UP, "s":DOWN, "a":LEFT, "d":RIGHT}
     delta = { #押されているキーkey/値:移動幅リスト[x,y]
-        "Up"    :[0, -1],
-        "Down"  :[0, +1],
-        "Left"  :[-1, 0],
-        "Right" :[+1,0],
-    }
+        "w":[0, -1],"s":[0, +1],"a":[-1, 0],"d":[+1,0],}
     try:
         if maze_bg[my+delta[key][1]][mx+delta[key][0]] == 0:
             my,mx = my+delta[key][1], mx+delta[key][0]
     except:
         pass
+
+
 
     cx,cy = mx*100+50,my*100+50
     canvas.coords("tori", cx, cy)
@@ -31,13 +32,13 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
 
-    canvas = tk.Canvas(root, width=1500, height=900, bg="black")
+    canvas = tk.Canvas(root, width=700, height=500, bg="black")
     canvas.pack()
-    maze_bg = mm.make_maze(15, 9) 
+    maze_bg = mm.make_maze(7, 5) 
     mm.show_maze(canvas, maze_bg)
     #print(maze_bg)
 
-    tori = tk.PhotoImage(file="fig/6.png")
+    tori = tk.PhotoImage(file="fig/8.png")
     mx, my = 1, 1
     cx,cy = mx*100+50,my*100+50
     canvas.create_image(cx,cy,image=tori,tag="tori")
