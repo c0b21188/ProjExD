d2c042d39be9e97604670bf7450477931754139e
yamamoto <C0B21188e9@edu.teu.ac.jp>
2022-06-28 15:49:38 +0900

    最後まで

--- ex03/maze.py
+++ ex03/maze.py
@@ -10,18 +10,22 @@ def key_up(event):
     key = ""
 
 def main_proc():
-    global cx, cy
+    global cx, cy, mx, my
     delta = { #押されているキーkey/値:移動幅リスト[x,y]
-        ""      :[0,0],
-        "Up"    :[0, -20],
-        "Down"  :[0, +20],
-        "Left"  :[-20, 0],
-        "Right" :[+20,0],
+        "Up"    :[0, -1],
+        "Down"  :[0, +1],
+        "Left"  :[-1, 0],
+        "Right" :[+1,0],
     }
+    try:
+        if maze_bg[my+delta[key][1]][mx+delta[key][0]] == 0:
+            my,mx = my+delta[key][1], mx+delta[key][0]
+    except:
+        pass
 
-    cx,cy = cx + delta[key][0], cy + delta[key][1]
+    cx,cy = mx*100+50,my*100+50
     canvas.coords("tori", cx, cy)
-    root.after(10, main_proc)
+    root.after(100, main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -34,11 +38,11 @@ if __name__ == "__main__":
     #print(maze_bg)
 
     tori = tk.PhotoImage(file="fig/6.png")
-    cx,cy = 300,400
+    mx, my = 1, 1
+    cx,cy = mx*100+50,my*100+50
     canvas.create_image(cx,cy,image=tori,tag="tori")
 
     key = ""
-
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
 
