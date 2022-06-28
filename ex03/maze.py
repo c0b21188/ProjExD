234ded10a91eb9fe2058e84aba8dc53adbb9796d
yamamoto <C0B21188e9@edu.teu.ac.jp>
2022-06-28 15:11:56 +0900

    7問目まで

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,26 @@
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym
+
+def key_up(event):
+    global key
+    key = ""
+
+def main_proc():
+    global cx, cy
+    delta = { #押されているキーkey/値:移動幅リスト[x,y]
+        ""      :[0,0],
+        "Up"    :[0, -20],
+        "Down"  :[0, +20],
+        "Left"  :[-20, 0],
+        "Right" :[+20,0],
+    }
+
+    cx,cy = cx + delta[key][0], cy + delta[key][1]
+    canvas.coords("tori", cx, cy)
+    root.after(10, main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -11,6 +32,13 @@ if __name__ == "__main__":
     tori = tk.PhotoImage(file="fig/6.png")
     cx,cy = 300,400
     canvas.create_image(cx,cy,image=tori,tag="tori")
+
+    key = ""
+
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
+
+    main_proc()
 
     root.mainloop()
 
